ca09dbc77d0891f6c9b8f3585891cfa837b90894
James Jeon <jkh911208@gmail.com>
2020-01-14 19:06:52 -0600

    Updates

---
+++ precache-manifest.7fe9070946c163c72cc82f92b88ad40d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301b6b3ffcc54e3fd9a032c084a11a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b2732ed78da0245610a",
+    "url": "/static/js/2.ab4fe633.chunk.js"
+  },
+  {
+    "revision": "9c8faab933b8fe02833aea8bf1f524cc",
+    "url": "/static/js/2.ab4fe633.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71634f9ab316a9886642",
+    "url": "/static/js/main.6c9df0ab.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
